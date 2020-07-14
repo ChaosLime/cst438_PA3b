d82cbeb8442d6cc7503c8e94a07366276915058a
Mitchell Saunders <d3thr3lic@gmail.com>
2020-07-14 13:29:30 -0700

    Formatting changes only. Functional code. Converted tabs to spaces.

--- src/main/java/cst438/Cst438Pa3bApplication.java
+++ src/main/java/cst438/Cst438Pa3bApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Cst438Pa3bApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Cst438Pa3bApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Cst438Pa3bApplication.class, args);
+  }
 
 }
